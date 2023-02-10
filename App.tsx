5ef04a11bec310f6d6a7702dbda76ddceae11e2d
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-09 23:47:28 -0300

    adjust android splash screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import {Home} from './src/pages/Home';
+import SplasScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplasScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" />
